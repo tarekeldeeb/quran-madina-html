cd8d6b1fb62234b1a72d03d4d4c604e53a4e560c
Tarek Eldeeb <tarekeldeeb@msn.com>
2023-04-28 16:19:11 +0300

    Added Progress Bar. Fixed Sura Names. Handled Aya1

--- buildDb.py
+++ buildDb.py
@@ -1,5 +1,5 @@
 import sqlite3
-import requests, os
+import requests, os, tqdm
 import json
 
 TMP = "temp"
@@ -17,6 +17,29 @@ def query(query):
 def get_aya_data(sura, ayah):
     result = query("select page_number, line_number, min_x, max_x from glyphs where sura_number={} and ayah_number={}".format(sura, ayah))
     return list(map(lambda x: list(x), result))
+
+def get_surah_name(sura_id):
+    sura_name = ["الفاتحة", "البقرة", "آل عمران",
+      "النساء", "المائدة", "الأنعام", "الأعراف", "الأنفال", "التوبة",
+      "يونس", "هود", "يوسف", "الرعد", "إبراهيم", "الحجر", "النحل",
+      "الإسراء", "الكهف", "مريم", "طه", "الأنبياء", "الحج", "المؤمنون",
+      "النور", "الفرقان", "الشعراء", "النمل", "القصص", "العنكبوت",
+      "الروم", "لقمان", "السجدة", "الأحزاب", "سبأ", "فاطر", "يس",
+      "الصافات", "ص", "الزمر", "غافر", "فصلت", "الشورى", "الزخرف",
+      "الدخان", "الجاثية", "الأحقاف", "محمد", "الفتح", "الحجرات", "ق",
+      "الذاريات", "الطور", "النجم", "القمر", "الرحمن", "الواقعة",
+      "الحديد", "المجادلة", "الحشر", "الممتحنة", "الصف", "الجمعة",
+      "المنافقون", "التغابن", "الطلاق", "التحريم", "الملك", "القلم",
+      "الحاقة", "المعارج", "نوح", "الجن", "المزمل", "المدثر", "القيامة",
+      "الإنسان", "المرسلات", "النبأ", "النازعات", "عبس", "التكوير",
+      "الانفطار", "المطففين", "الانشقاق", "البروج", "الطارق", "الأعلى",
+      "الغاشية", "الفجر", "البلد", "الشمس", "الليل", "الضحى", "الشرح",
+      "التين", "العلق", "القدر", "البينة", "الزلزلة", "العاديات",
+      "القارعة", "التكاثر", "العصر", "الهمزة", "الفيل", "قريش",
+      "الماعون", "الكوثر", "الكافرون", "النصر", "المسد", "الإخلاص",
+      "الفلق", "الناس"
+    ]
+    return "سورة " + sura_name[sura_id]
 
 if __name__ == '__main__':
     try:
@@ -36,44 +59,51 @@ if __name__ == '__main__':
     except OSError as error:
         print("Skipping download ..")
     json_header = '{"title": "مصحف المدينة الإصدار القديم - مجمع الملك فهد لطباعة المصحف الشريف",\
-	    "published": 1985}'
+        "published": 1985,\
+        "font": "https://fonts.googleapis.com/css?family=Amiri Quran"}'
     suras = []
-    with open(os.path.join(TMP,TXT), encoding="utf8") as f:
-        for line in f:
-            tokens = line.strip().split('|')
-            if len(tokens) == 3:
-                sura, aya, aya_text = tokens
-                aya_text = aya_text
-                sura = int(sura)
-                aya = int(aya)
-                if len(suras)<sura:
-                    suras.append({"name":"سورة ..", "ayas":[]}) #Add a Sura
-                #Add an Aya
-                if aya>1:
-                    prev_aya_data = ayah_data
-                else:
-                    prev_aya_data = ""
-                ayah_data = get_aya_data(sura, aya)
-                ayah_data = list(filter(lambda a: a[3]-a[2] > 30, ayah_data))
-                page = ayah_data[0][0]
-                ## TODO: Find a way to break ayas into parts.
-                lines = {}
-                parts = []
-                for glyph in ayah_data:
-                    if str(glyph[1]) not in lines:
-                        lines.update({str(glyph[1]):1})
+    print("Processing ..")
+    with tqdm.tqdm(total=os.path.getsize(os.path.join(TMP,TXT))) as pbar:
+        with open(os.path.join(TMP,TXT), encoding="utf8") as f:
+            for aya_line in f:
+                pbar.update(len(aya_line.encode('utf-8')))
+                tokens = aya_line.strip().split('|')
+                if len(tokens) == 3:
+                    sura, aya, aya_text = tokens
+                    aya_text = aya_text
+                    sura = int(sura)
+                    aya = int(aya)
+                    #Add a Sura
+                    if len(suras)<sura:
+                        suras.append({"name": get_surah_name(sura-1), "ayas":[]})
+                    #Add an Aya
+                    if aya>1:
+                        prev_aya_data = ayah_data
                     else:
-                        lines[str(glyph[1])] = lines[str(glyph[1])] + 1
-                words = 0
-                for l in lines.keys():
-                    parts.append({"l":int(l), "t":" ".join(aya_text.split()[words:(words+lines[l])]), "o":20, "s": .23})
-                    words = words + lines[l]
-                ## TODO: Find a way to calculate the stretching factor for each line
-                aya_obj = {"p":page, "r":parts}
-                suras[sura-1]["ayas"].append(aya_obj)
-    f.close()
+                        prev_aya_data = ""
+                    ayah_data = get_aya_data(sura, aya)
+                    ayah_data = list(filter(lambda a: a[3]-a[2] > 30, ayah_data))
+                    page = ayah_data[0][0]
+                    lines = {}
+                    parts = [] # Break each ayah into parts/lines
+                    for glyph in ayah_data:
+                        if str(glyph[1]) not in lines:
+                            lines.update({str(glyph[1]):1})
+                        else:
+                            lines[str(glyph[1])] = lines[str(glyph[1])] + 1
+                    if aya==1 and sura!=1 and sura!=9:
+                        skip_words = 4 #Skip Basmala from first aya
+                    else:
+                        skip_words = 0 
+                    for l in lines.keys():
+                        parts.append({"l":int(l), "t":" ".join(aya_text.split()[skip_words:(skip_words+lines[l])]), "o":20, "s": .23})
+                        skip_words = skip_words + lines[l]
+                    ## TODO: Find a way to calculate the stretching factor for each line
+                    aya_obj = {"p":page, "r":parts}
+                    suras[sura-1]["ayas"].append(aya_obj)
+        f.close()
     j = json.loads(json_header)
     j.update({"suras":suras})
-    json_file = open('Madina.json', 'w', encoding="utf-8")
-    json.dump(j, json_file, ensure_ascii = False)
+    json_file = open('Madina-Amiri.json', 'w', encoding="utf-8")
+    json.dump(j, json_file, ensure_ascii = False, indent=2)
     json_file.close()
