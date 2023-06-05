829ef98af31fe4b392f255060c18a516eec6580e
Tarek Eldeeb <tarekeldeeb@msn.com>
2023-06-05 15:50:40 +0300

    Applying PyLint warnings

--- build_db.py
+++ build_db.py
@@ -27,15 +27,15 @@ STRETCH_ROUNDING = 3
 
 def _query(query):
     conn = sqlite3.connect(os.path.join(TMP,DB))
-    c = conn.cursor()
-    c.execute(query)
-    result = c.fetchall()
-    c.close()
+    cursor = conn.cursor()
+    cursor.execute(query)
+    result = cursor.fetchall()
+    cursor.close()
     return result
 
 def _get_aya_data(sura, ayah):
-    result = _query("select page_number, line_number, min_x, max_x from glyphs\
-                    where sura_number={} and ayah_number={}".format(sura, ayah))
+    result = _query(f'select page_number, line_number, min_x, max_x from glyphs '
+                    f'where sura_number={sura} and ayah_number={ayah}')
     return list(map(lambda x: list(x), result))
 
 def _get_surah_name(sura_id):
@@ -61,18 +61,18 @@ def _get_surah_name(sura_id):
     ]
     return "سورة " + sura_name[sura_id]
 
-def _update_html_text(wd, text, skip):
-    wd.execute_script("document.getElementById('test').textContent = '{}'".format(text))
+def _update_html_text(web_driver, text):
+    web_driver.execute_script(f'document.getElementById(\'test\').textContent = \'{text}\'')
 
-def _get_width(wd):
-    return wd.execute_script("return document.getElementById('test').getBoundingClientRect().width")
+def _get_width(web_driver):
+    return web_driver.execute_script("return document.getElementById('test').getBoundingClientRect().width")
 
-def _update_line_data(suras, parts, page, sura, aya, current_line, current_line_width):
+def _update_line_data(work_pointer):
     """Calculate the stretching factor (s), then apply to all previous line
     parts and updates their offsets (o).
     Finally, the returns (s,o) for the last part to be written later.
 
-    Args:
+    Args: work_pointer
         suras (JSON): with complete Ayas
         parts (list): parts of an Aya
         sura (int): Current Surah number
@@ -80,23 +80,23 @@ def _update_line_data(suras, parts, page, sura, aya, current_line, current_line_
         current_line (int): Line Number to update in page
         current_line_width (float): Sum of widths of all line parts
     """
-    LOOK_BACK = 5
+    suras, parts, page, sura, aya, current_line, current_line_width = work_pointer
+    look_back = 5
     if current_line_width<20:
         _save_json("", suras)
-        raise Exception("Problem with [short] aya={} of sura={} at line={}"\
-                        .format(aya, sura, current_line))
+        raise ValueError(f'Problem with [short] aya={aya} of sura={sura} at line={current_line}')
     stretch = LINE_WIDTH/(current_line_width) if page>2 else 1
-    aya_look_back = aya-LOOK_BACK if aya>LOOK_BACK else 1
+    aya_look_back = aya-look_back if aya>look_back else 1
     offset = 0
     if aya > 1:
         search_ayas = suras[sura-1]['ayas'][aya_look_back-1:aya-1]
-        for a in search_ayas:
-            for r in a['r']:
-                if r['l'] == int(current_line):
-                    tmp = r['o']*stretch
-                    r['o'] = math.ceil(offset)
+        for search_aya in search_ayas:
+            for search_aya_part in search_aya['r']:
+                if search_aya_part['l'] == int(current_line):
+                    tmp = search_aya_part['o']*stretch
+                    search_aya_part['o'] = math.ceil(offset)
                     offset = offset + tmp
-                    r['s'] = round(stretch, STRETCH_ROUNDING)
+                    search_aya_part['s'] = round(stretch, STRETCH_ROUNDING)
     if len(parts) > 0:
         parts[-1]["o"] = math.ceil(offset*stretch)
         parts[-1]["s"] = round(stretch, STRETCH_ROUNDING)
@@ -105,52 +105,55 @@ def _update_line_data(suras, parts, page, sura, aya, current_line, current_line_
 def _save_json(json_header, suras):
     j = json.loads(json_header)
     j.update({"suras":suras})
-    json_file = open(DB_JSON_FILE, 'w', encoding="utf-8")
-    json.dump(j, json_file, ensure_ascii = False, indent=2)
-    print("Saved: {}".format(DB_JSON_FILE) )
-    json_file.close()
+    with open(DB_JSON_FILE, 'w', encoding="utf-8") as json_file:
+        json.dump(j, json_file, ensure_ascii = False, indent=2)
+    print(f'Saved: {DB_JSON_FILE}')
 
 def run():
+    """Runs the build_db module
+    """
     try:
         os.mkdir(TMP)
         # Start with downloading the Glyph DB
-        URL = "https://raw.githubusercontent.com/murtraja/quran-android-images-helper"\
+        url = "https://raw.githubusercontent.com/murtraja/quran-android-images-helper"\
             "/master/static/databases/" + DB.lower()
-        response = requests.get(URL)
-        open(os.path.join(TMP,DB), "wb").write(response.content)
+        response = requests.get(url, timeout=500)
+        with open(os.path.join(TMP,DB), "wb") as file_db:
+            file_db.write(response.content)
         print("Downloaded Quran DataBase")
         #
         # Then download the text from Tanzil.net
         txt_url = "https://tanzil.net/pub/download/index.php?marks=true&sajdah=true"\
                     "&rub=true&tatweel=true&quranType=uthmani&outType=txt-2&agree=true"
-        req = requests.get(txt_url, allow_redirects=True)
+        req = requests.get(txt_url, timeout=500, allow_redirects=True)
         text = req.content
-        open(os.path.join(TMP,TXT), "wb").write(text)
+        with open(os.path.join(TMP,TXT), "wb") as file_txt:
+            file_txt.write(text)
         print("Downloaded Quran Text file.")
-    except OSError as error:
+    except OSError:
         print("Skipping download ..")
     #
     # Finally Load the Html Template and Driver
     chrome_options = Options()
     chrome_options.add_experimental_option("excludeSwitches", ['enable-automation'])
-    wd = webdriver.Chrome(options=chrome_options)
+    web_driver = webdriver.Chrome(options=chrome_options)
     dir_path = os.path.dirname(os.path.realpath(__file__))
-    wd.get("file://" + os.path.join(dir_path,WIDTH_TEST_HTML))
+    web_driver.get("file://" + os.path.join(dir_path,WIDTH_TEST_HTML))
     # Lets start building the json output ..
-    json_header = '\
+    json_header = f'\
         {{"title": "مصحف المدينة الإصدار القديم - مجمع الملك فهد لطباعة المصحف الشريف",\
         "published": 1985,\
         "font_family": "Amiri Quran",\
         "font_url":\
             "https://fonts.gstatic.com/s/amiriquran/v7/_Xmo-Hk0rD6DbUL4_vH8Zp5v5i2ssg.woff2",\
         "font_size": 24,\
-        "line_width": {}}}'.format(LINE_WIDTH)
+        "line_width": {LINE_WIDTH}}}'
     suras = []
     page = aya = current_line_width = current_line = 0
     print("Processing ..")
     with tqdm.tqdm(total=os.path.getsize(os.path.join(TMP,TXT))) as progress_bar:
-        with open(os.path.join(TMP,TXT), encoding="utf8") as f:
-            for aya_line in f:
+        with open(os.path.join(TMP,TXT), encoding="utf8") as file_txt:
+            for aya_line in file_txt:
                 progress_bar.update(len(aya_line.encode('utf-8')))
                 tokens = aya_line.strip().split('|')
                 if len(tokens) == 3:
@@ -170,7 +173,7 @@ def run():
                         current_line = 1 if page>1 else 3 if page==1 else 2 #first line in page
                         current_line_width = 0
                         page = ayah_data[0][0]
-                        new_page = True if page>0 else False
+                        new_page = page>0
                     else:
                         new_page = False
                     lines = {} # Key=LineNumber, Value=Number of glyphs
@@ -184,29 +187,31 @@ def run():
                         skip_words = 4 #Skip Basmala from first aya. TODO: Add Basmala Manually
                     else:
                         skip_words = 0
-                    for l in lines.keys():
-                        if l != str(current_line): #new line
+                    for line in lines:
+                        if line != str(current_line): #new line
                             #Override (o,s) of line parts
                             if new_page:
-                                suras, parts = _update_line_data(suras, parts, page, prev_sura,
-                                                               prev_aya, prev_line, prev_line_width)
+                                work_pointer = (suras, parts, page, prev_sura,
+                                                prev_aya, prev_line, prev_line_width)
                             else:
-                                suras, parts = _update_line_data(suras, parts, page, sura, aya,
-                                                               current_line, current_line_width)
+                                work_pointer = (suras, parts, page, sura, aya,
+                                                current_line, current_line_width)
+                            suras, parts = _update_line_data(work_pointer)
                             current_line_width = 0
-                        current_line = int(l)
-                        aya_text_part = " ".join(aya_text.split()[skip_words:(skip_words+lines[l])])
-                        if l == list(lines.keys())[-1]:
-                            aya_text_part = aya_text_part + " \uFD3F{}\uFD3E".format(aya)
-                        _update_html_text(wd, aya_text_part, 0)
-                        aya_part_width = _get_width(wd)
+                        current_line = int(line)
+                        aya_text_part = " ".join(aya_text.split()
+                                                 [skip_words:skip_words+lines[line]])
+                        if line == list(lines.keys())[-1]:
+                            aya_text_part = aya_text_part + f' \uFD3F{aya}\uFD3E'
+                        _update_html_text(web_driver, aya_text_part)
+                        aya_part_width = _get_width(web_driver)
                         current_line_width = current_line_width + aya_part_width
-                        parts.append({"l":int(l), "t":aya_text_part, "o":aya_part_width, "s": 1.0})
-                        skip_words = skip_words + lines[l]
+                        parts.append({"l":int(line), "t":aya_text_part, 
+                                      "o":aya_part_width, "s": 1.0})
+                        skip_words = skip_words + lines[line]
                     suras[sura-1]["ayas"].append({"p":page, "r":parts}) # New completed ayah
-        f.close()
     print("Closing Chrome ..")
-    wd.close()
+    web_driver.close()
     _save_json(json_header, suras)
 
 if __name__ == '__main__':
