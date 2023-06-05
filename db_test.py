172564be7e670a88c2a694f44ae3108ea75c58bf
Tarek Eldeeb <tarekeldeeb@msn.com>
2023-06-05 14:00:22 +0300

    Added a basic Unit Test

---
+++ db_test.py
@@ -0,0 +1,26 @@
+"""Testing the Generated JSON DB including all Quran data
+"""
+import unittest
+import json
+from build_db import DB_JSON_FILE
+
+
+class Test(unittest.TestCase):
+    """Testing the Generated JSON DB including all Quran data
+    """
+    db = json.load(open(DB_JSON_FILE, 'r', encoding="utf-8"))
+    def test_0_db_fields(self):
+        """Test all JSON header fields if exist
+        """
+        for field in ['title','published','font_family','font_url',
+                      'font_size','line_width','suras']:
+            # check if the obtained field is null or not
+            self.assertIsNotNone(self.db[field])  # null will fail the test
+
+    def test_1_sura_count(self):
+        """Test Sura Count
+        """
+        self.assertEqual(len(self.db['suras']), 114, "Missing Suras!")
+
+if __name__ == '__main__':
+    unittest.main()
