bf90d3baf7fb2a115314cb67c23f3fd8cf2bb7f7
Tarek Eldeeb <tarekeldeeb@msn.com>
2023-07-11 01:39:55 +0300

    Pylint fixes

--- build_all.py
+++ build_all.py
@@ -1,7 +1,14 @@
+"""Iterates over build_db.py with multiple configurations
+   Fonts covered: Uthman, Amiri Quran Colored
+   Font Sizes: Multiple
+
+   Returns:
+        None
+"""
 from multiprocessing import Pool
 from argparse import Namespace
 from tqdm import tqdm
-from build_db import run, DEFAULTS
+from build_db import run, DEFAULTS, REPO
 
 amiri24 = DEFAULTS.copy()
 amiri24['font_size']=24
@@ -9,7 +16,7 @@ amiri24['line_width']=400
 
 uthman16 = DEFAULTS.copy()
 uthman16['font_family'] = 'Uthman'
-uthman16['font_url'] = 'https://raw.githubusercontent.com/tarekeldeeb/quran-madina-html-no-images/main/assets/fonts/UthmanTN_v2-0.woff2'
+uthman16['font_url'] = REPO+"assets/fonts/UthmanTN_v2-0.woff2"
 
 uthman14 = uthman16.copy()
 uthman14['font_size'] = 14
@@ -20,4 +27,4 @@ cfgs = [ DEFAULTS, amiri24, uthman16, uthman14 ]
 if __name__ == '__main__':
     #TODO: Fix the nested progress bar
     with Pool(4) as p:
-      r = list(tqdm(p.imap(run, list(map(lambda x: Namespace(**x) ,cfgs))), total=len(cfgs)))
+        r = list(tqdm(p.imap(run, list(map(lambda x: Namespace(**x) ,cfgs))), total=len(cfgs)))
