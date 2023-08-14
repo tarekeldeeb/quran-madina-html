a2be2f4eda6f85be9bf419e0f18980ff49034bd0
Tarek Eldeeb <tarekeldeeb@msn.com>
2023-08-14 23:13:51 +0300

    Fixed the second scan. Still fails with some null values

--- src/db/build_db.py
+++ src/db/build_db.py
@@ -33,7 +33,7 @@ TXT = "Uthmani.txt"
 TEST_HTML_TEMPLATE = "src/template/part_width_test.html"
 CDN = 'https://www.unpkg.com/quran-madina-html/'
 REPO = CDN #'https://raw.githubusercontent.com/tarekeldeeb/quran-madina-html-no-images/main/'
-DEFAULTS = {'name':'Madina', 'published': 1985,
+DEFAULTS = {'name':'Madina', 'published': 1405,
            'title':"مصحف المدينة الإصدار القديم - مجمع الملك فهد لطباعة المصحف الشريف",
            'font_family':'Amiri Quran Colored',
            'font_url':REPO+'assets/fonts/AmiriQuranColored.woff2',
@@ -41,30 +41,34 @@ DEFAULTS = {'name':'Madina', 'published': 1985,
 # Runtime globals
 DBG_LINE_WIDTHS = []
 
-class Control:
-    """Aggregates all needed Controls to run"""
+class DbBuilder:
+    """Aggregates all needed DbBuilders to run"""
     web_driver = None #type:ignore
     cursor = None #type:ignore
     progress = None #type:ignore
     cfg = None #type:ignore
     base_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), "../..")
     json_helper = None
+    error_logger = None
 
     @staticmethod
     def get_test_filename():
         """returns the Html file name to be used for testing/web_driver"""
-        suffix = f'-{Control.cfg.font_family}-{Control.cfg.font_size}' # type: ignore
-        return os.path.join(Control.base_dir,"src/db/test"+suffix+".html")
+        suffix = f'-{DbBuilder.cfg.font_family}-{DbBuilder.cfg.font_size}' # type: ignore
+        return os.path.join(DbBuilder.base_dir,"src/db/test"+suffix+".html")
     @staticmethod
     def print_dbg_widths():
-        print(f'Non-Stretched Line widths:: Avg: {sum(DBG_LINE_WIDTHS) / len(DBG_LINE_WIDTHS)}, '
-            f'Max:{max(DBG_LINE_WIDTHS)}, Min:{min(DBG_LINE_WIDTHS)}')
+        if len(DBG_LINE_WIDTHS):
+            print(f'Non-Stretched Line widths:: Avg: {sum(DBG_LINE_WIDTHS) / len(DBG_LINE_WIDTHS)}, '
+                f'Max:{max(DBG_LINE_WIDTHS)}, Min:{min(DBG_LINE_WIDTHS)}')
     @staticmethod
     def run(cfg):
-        Control.cfg = cfg
-        Control.cursor = LineCursor(0, 0)
-        Control.json_helper = JsonHelper(cfg)
-        Control.html_helper = HtmlHelper()
+        DbBuilder.cfg = cfg
+        DbBuilder.cursor = LineCursor(0, 0)
+        DbBuilder.json_helper = JsonHelper(cfg)
+        DbBuilder.html_helper = HtmlHelper()
+        DbBuilder.error_logger = ErrorLogger()
+
         """Runs the build_db module
         """
         try:
@@ -87,29 +91,30 @@ class Control:
             print("Downloaded Quran Text file.")
         except OSError:
             print("Skipping download ..")
-        Control.html_helper.make_html_test()
+        DbBuilder.html_helper.make_html_test()
         #
         # Finally Load the Html Template and Driver
         chrome_options = Options()
         chrome_options.add_experimental_option("excludeSwitches", ['enable-automation'])
-        Control.web_driver = webdriver.Chrome(options=chrome_options)
-        test_url = "file://" + Control.get_test_filename()
-        Control.web_driver.get(test_url)
-        Control.html_helper.ensure_page_has_loaded(test_url)
+        DbBuilder.web_driver = webdriver.Chrome(options=chrome_options)
+        test_url = "file://" + DbBuilder.get_test_filename()
+        DbBuilder.web_driver.get(test_url)
+        DbBuilder.html_helper.ensure_page_has_loaded(test_url)
         suras = []
         print("Processing ..")
         with tqdm.tqdm(total=os.path.getsize(os.path.join(TMP,TXT)), leave=False) as progress_bar:
-            Control.progress = progress_bar
+            DbBuilder.progress = progress_bar
             with open(os.path.join(TMP,TXT), encoding="utf8") as file_txt:
-                suras = Mushaf(Control.cfg, file_txt).process()
+                suras = Mushaf(DbBuilder.cfg, file_txt).process()
         print("Closing Chrome ..")
-        Control.web_driver.close()
-        os.remove(Control.get_test_filename())
-        Control.json_helper.save_json(suras)
-        Control.print_dbg_widths()
+        DbBuilder.web_driver.close()
+        os.remove(DbBuilder.get_test_filename())
+        DbBuilder.json_helper.save_json(suras)
+        DbBuilder.error_logger.flush()
+        DbBuilder.print_dbg_widths()
 
 class LineCursor:
-    """Part of the global Control, points to the current point in DB"""
+    """Part of the global DbBuilder, points to the current point in DB"""
     def __init__(self, page, line):
         self.page = page
         self.line = line
@@ -130,17 +135,20 @@ class QuranLine:
     def __init__(self, page, parts):
         self.page = page
         self.parts = parts
+    def previous_widths(self, parts)-> int:
+        """Offset each aya by sum of widths of previous ayas on the same line"""
+        if len(parts)==0: return 0
+        elif len(parts)==1: return math.ceil(parts[0].width)
+        return math.ceil(reduce(lambda a,b: (a.width if hasattr(a,'width') else a)+b.width, parts))
     def update_parts(self, line_width):
+        """Apply stretch and offset calculations to all line parts"""
         initial_width = 0
         for part in self.parts:
             initial_width = initial_width + part.width
         stretch = line_width/initial_width
-        for part_index, part in self.parts:
+        for part_index, part in enumerate(self.parts):
             part.stretch = round(stretch, self.STRETCH_ROUNDING) if self.page>2 else -1
-            part.offset = math.ceil(reduce(
-                lambda a,b: a.width+b.width,
-                self.parts[0:part_index-1],
-                0))
+            part.offset = self.previous_widths(self.parts[0:part_index])
         return self.parts
 
 class Part:
@@ -155,7 +163,7 @@ class Part:
         """Returns a JSON-ready Object"""
         return {"l":self.line, "t":self.text,"o":self.offset, "s": self.stretch}
 
-class Ayah(Control):
+class Ayah(DbBuilder):
     """Quran Aya"""
     def __init__(self, sura_index, index, text):
         self.sura_index = sura_index
@@ -165,10 +173,25 @@ class Ayah(Control):
         self.line_start = None
         self.line_end = None
         self.json = None
-        if "Amiri" in Control.cfg.font_family:
+        if "Amiri" in DbBuilder.cfg.font_family:
             self.text = text + f' \u06DD{index}'
         else:
             self.text = text.replace("ٱ", "ا") + f' \uFD3F{index}\uFD3E'
+    @classmethod
+    def from_part_data(cls, text, page, line, offset, stretch):
+        instance = cls(0,0,text)
+        instance.page = page
+        instance.line_start = line
+        part = Part(line, text, 100)
+        part.offset = offset
+        part.stretch = stretch
+        part.text = text
+        instance.parts = [part]
+        return instance
+    def update_json(self):
+        """returns the json string of this object"""
+        self.json = {"p": self.page, "r": list(map(lambda part: part.to_json(), self.parts))}
+        return self.json
     def process(self):
         """Process the aya to create a json-ready object"""
         ayah_data = DbReader.get_aya_data(self.sura_index+1, self.index)
@@ -186,19 +209,18 @@ class Ayah(Control):
         else:
             skip_words = 0
         if len(ayah_data) != len(self.text.split())-skip_words:
-            print(f'Mismatch at {self.sura_index}-{self.index}: '
+            DbBuilder.error_logger.add_message(f'Mismatch at {self.sura_index}-{self.index}: '
                   f'Glyphs={len(ayah_data)} for:{self.text}')
         for line_index, line in words_in_lines.items():
             aya_text_part = " ".join(self.text.split()
                                         [skip_words:skip_words+line])
-            aya_part_width = Control.html_helper.get_width(aya_text_part)
+            aya_part_width = DbBuilder.html_helper.get_width(aya_text_part)
             self.parts.append(Part(int(line_index), aya_text_part, aya_part_width))
             skip_words = skip_words + line
-        Control.cursor.page = self.page
-        Control.cursor.line = self.line_end
-        Control.progress.update(len(self.text.encode('utf-8'))+2)
-        self.json = {"p": self.page, "r": list(map(lambda part: part.to_json(), self.parts))}
-        return self.json
+        DbBuilder.cursor.page = self.page
+        DbBuilder.cursor.line = self.line_end
+        DbBuilder.progress.update(len(self.text.encode('utf-8'))+2)
+        return self
 
 class Surah:
     """Quran Surah/Chapter"""
@@ -235,29 +257,30 @@ class Surah:
             return "🙮 " + sura_name + " 🙬"
         return sura_name
     def get_aya01(self):
-        page = Control.cursor.page
-        line = Control.cursor.line
+        page = DbBuilder.cursor.page
+        line = DbBuilder.cursor.line
         title_page = 1 if self.index == 0 else page+1 if line==15 else page
         title_line = 1 if  line==15 or self.index<=1 else line+1
         bsm_page = title_page+1 if title_line==15 else title_page
         bsm_line = 1 if title_line==15 else title_line+1
-        aya01 = [{ "p": title_page,
-                   "r": [{"l": title_line,
-                          "t": Surah.get_surah_name(self.index, True), "o": 0, "s": -1}]},
-                { "p": bsm_page,
-                  "r": [{"l": bsm_line, "t": "﷽", "o": 0, "s": -1}]}]
+        aya0 = Ayah.from_part_data(Surah.get_surah_name(self.index, True),
+                                    title_page, title_line, 0, -1)
+        aya1 = Ayah.from_part_data("﷽", bsm_page, bsm_line, 0, -1)
+        aya_empty = Ayah.from_part_data("", title_page, title_line, 0, -1)
+
         if self.index == 0:
-            aya01.pop()
-            aya01.insert(0, { "p": title_page,  #Empty aya: Ensures correct numbers
-                        "r": [{"l": title_page, "t": "", "o": 0, "s": -1}]}) 
-        return aya01
+            return [aya_empty,aya0]
+        return [aya0, aya1]
     def process(self):
         """Process the surah to create a json-ready object"""
         self.ayas = self.get_aya01()
         for line in self.lines:
             self.ayas.append(Ayah(self.index, int(line[0]), line[1]).process())
+        return self
+    def update_json(self):
+        """returns the json string of this object"""
         self.json = {"name": self.name,
-                     "ayas": self.ayas}
+                "ayas": list(map(lambda aya: aya.update_json(), self.ayas))}
         return self.json
     def align_lines(self):
         """Ensure All Line widths are equal
@@ -269,19 +292,22 @@ class Surah:
         aya_index = 0
         while cursor <= LineCursor(self.ayas[-1].page, self.ayas[-1].line_end):
             aya = self.ayas[aya_index]
-            for part_index, part in aya.parts:                         
+            for part_index, part in enumerate(aya.parts):                         
                 if part.offset == None: # Skip already processed Line
                     if part_index<len(aya.parts)-1: # Complete Line(s)
-                        part = QuranLine(aya.page, part).update_parts(Control.cfg.line_width)
+                        part = QuranLine(aya.page, [part]).update_parts(DbBuilder.cfg.line_width)
                     else:
                         line_parts = [part]
                         search_index = aya_index +1
-                        while self.ayas[search_index].parts[0].line == part.line:
+                        while search_index<len(self.ayas) and\
+                            self.ayas[search_index].parts[0].line == part.line:
                             line_parts.append(self.ayas[search_index].parts[0])
                             search_index = search_index + 1
                         line_parts = QuranLine(aya.page,line_parts)\
-                            .update_parts(Control.cfg.line_width)
+                            .update_parts(DbBuilder.cfg.line_width)
                 cursor.nextLine()
+            aya_index = aya_index + 1
+        return self.update_json()
 
 class Mushaf:
     """Quran book, has a configuration parameter to define the rendering font"""
@@ -289,6 +315,7 @@ class Mushaf:
         self.cfg = cfg
         self.file_txt = file_txt
         self.suras = []
+        self.suras_json = []
     def process(self):
         """Process the Mushaf to create a json-ready object"""
         sura_lines = []
@@ -302,9 +329,9 @@ class Mushaf:
                     self.suras.append(Surah(len(self.suras), sura_lines).process())
                     sura_lines = [(aya, aya_text)]
         self.suras.append(Surah(len(self.suras), sura_lines).process())  #Last Sura
-        for sura in self.suras:
-            sura.align_lines()
-        return self.suras
+        for sura in self.suras: #second scan for alignment and json conversion
+            self.suras_json.append(sura.align_lines())
+        return self.suras_json
 
 class DbReader:
     """SQLITE DB Handler"""
@@ -352,10 +379,10 @@ class HtmlHelper:
     """A Collection of Html Helper Functions"""
     @staticmethod
     def make_html_test():
-        font = Control.cfg.font_family
-        font_url = Control.cfg.font_url
-        font_sz = Control.cfg.font_size
-        line_width = Control.cfg.line_width
+        font = DbBuilder.cfg.font_family
+        font_url = DbBuilder.cfg.font_url
+        font_sz = DbBuilder.cfg.font_size
+        line_width = DbBuilder.cfg.line_width
         with open(TEST_HTML_TEMPLATE, "r", encoding="utf8") as template:
             soup = BeautifulSoup(template.read(), 'html.parser')
         style_elem = soup.find("style").string # type: ignore
@@ -365,17 +392,17 @@ class HtmlHelper:
         style_elem = style_elem.replace("260", str(line_width))
         style_elem = style_elem.replace("16", str(font_sz))
         soup.find("style").string = style_elem # type: ignore
-        with open(Control.get_test_filename(), 'w', encoding="utf8") as file:
+        with open(DbBuilder.get_test_filename(), 'w', encoding="utf8") as file:
             file.write(str(soup))
     @staticmethod
     def update_html_text(text):
-        Control.web_driver.execute_script(f'document.getElementById(\'test\')' \
+        DbBuilder.web_driver.execute_script(f'document.getElementById(\'test\')' \
                                           f'.textContent = \'{text}\'')
     @staticmethod
     def ensure_page_has_loaded(url):
         time.sleep(5) # Hack!
         try:
-            WebDriverWait(Control.web_driver, 5).until(EC.url_to_be(url))
+            WebDriverWait(DbBuilder.web_driver, 5).until(EC.url_to_be(url))
         except TimeoutException:
             pass
             #print(f'URL ({url}) was not rendered with in allocated time')
@@ -383,8 +410,19 @@ class HtmlHelper:
     @staticmethod
     def get_width(aya_text):
         HtmlHelper.update_html_text(aya_text)
-        return Control.web_driver.execute_script("return document.getElementById('test')"
+        return DbBuilder.web_driver.execute_script("return document.getElementById('test')"
                                         ".getBoundingClientRect().width")
+
+class ErrorLogger:
+    def __init__(self):
+        self.logs = []
+    def add_message(self, line):
+        self.logs.append(line)
+    def flush(self):
+        if len(self.logs):
+            print(f"Errors Encountered while processing:")
+            for index, line in enumerate(self.logs):
+                print(f"| E{index}:\t{line}")
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Build JSON DB for HTML Quran Rendering.')
@@ -402,4 +440,4 @@ if __name__ == '__main__':
                         help="Font Size to render")
     parser.add_argument("--line_width", type=int, required=False, default=DEFAULTS["line_width"],
                         help="Page width to render")
-    Control.run(parser.parse_args())
+    DbBuilder().run(parser.parse_args())
